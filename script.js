497c207d105f52c1a30fed1edde64f5468786ffa
Prashant Lalwani <99117462+PrashantLalwani8289@users.noreply.github.com>
2022-11-09 17:00:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,333 @@
+let  myJokes = [
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 22,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Today I learned that changing random stuff until your program works is \"hacky\" and a \"bad coding practice\" but if you do it fast enough it's \"Machine Learning\" and pays 4x your current salary.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 33,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "ASCII silly question, get a silly ANSI.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 25,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "I have a joke about Stack Overflow, but you would say it's a duplicate.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 301,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Your momma is so fat, you need to switch to NTFS to store a picture of her.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "id": 55,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Judge: \"I sentence you to the maximum punishment...\"\nMe (thinking): \"Please be death, please be death...\"\nJudge: \"Learn Java!\"\nMe: \"Damn.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 45,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 12,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 27,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "The six stages of debugging:\n1. That can't happen.\n2. That doesn't happen on my machine.\n3. That shouldn't happen.\n4. Why does that happen?\n5. Oh, I see.\n6. How did that ever work?",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 123,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "\"Can I tell you a TCP joke?\"\n\"Please tell me a TCP joke.\"\n\"OK, I'll tell you a TCP joke.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 57,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "I have a fish that can breakdance! Only for 20 seconds though, and only once.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 237,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "Doctor: \"I have some news about your baby.\"\nParents: \"Don't tell us the gender, we want to keep it a surprise.\"\nDoctor: \"Oh I get it, you're those type of people. Okay, well IT is not breathing.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 138,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "My grandfather says I'm too reliant on technology.\nI called him a hypocrite and unplugged his life support.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 178,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "My little daughter came to me all excited, saying \"Daddy! Daddy! Guess how old I'll be in June!\"\n\"Oh I don't know princess, why don't you tell me?\" I said. She gave me a huge smile and held up four fingers.\nIt's now three hours later, police have joined in and she still won't say where she got them.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 165,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "I'll never forget my Granddad's last words to me just before he died. \"Are you still holding the ladder?\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 208,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "Hey girl are you a school? Because I want to shoot some kids up inside of you.",
+        "flags": {
+            "nsfw": true,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "safe": false,
+        "id": 269,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "My ex had an accident. I told the paramedics the wrong blood type for her. She'll finally experience what rejection is really like.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 152,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "I didn't vaccinate my 10 kids and the one that survived is fine!",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 96,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "I don't know where I stand on abortion. I like killing babies, but I don't like giving women a choice.",
+        "flags": {
+            "nsfw": true,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": true,
+            "explicit": true
+        },
+        "id": 228,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "My girlfriend's dog died, so I tried to cheer her up by getting her an identical one. It just made her more upset. She screamed at me, \"What am I supposed to do with two dead dogs?\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": false,
+        "id": 275,
+        "lang": "en"
+    }
+
+
+]
+
+let index = Math.floor(Math.random() *(myJokes.length - 1) );
+let joke = document.querySelector(".jokes");
+joke.textContent = myJokes[index].joke;
+
+let btn = document.querySelector(".btn-main");
+btn.addEventListener("click", () => {
+    location.reload();
+});
